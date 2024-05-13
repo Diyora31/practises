ee7e1e564ca77585bcbf33b9538517d5e199adb4
Diyora31 <asilovad7>
2024-05-13 14:22:12 +0500

    done

---
+++ main.js
@@ -0,0 +1,50 @@
+// task1
+
+let a=+prompt('son kiriting')
+
+let b=+prompt('2 ci sonni kiriting')
+
+console.log(`${a+b}`);
+
+// task2
+
+// let num = (Math.random()*100)
+
+// if (num % 2 == 0) {
+//     console.log('even');
+// }
+// else if (num % 2 > 0) {
+//     console.log('odd');
+// }
+
+
+// task3
+
+// let arr = ['fizika', 'huquq', 'kimyo', 'IT', 'biologiya']
+
+// let words = []
+
+// for (let word of arr) {
+//     if (word.length < 5) {
+//         words.push(word)
+//     }
+// }
+
+// task4
+
+// let a = '12'
+
+// console.log((a !== typeof numberdata));
+
+// task5
+
+// let arr=[82,15,6,23,73]
+// let res=[]
+// for(let i = 0; i > num.length; i++){
+//     if (i%2==0){
+//         res.push(arr[i])
+//     }
+// }
+
+
+
